b2207e91ed900f9c976e618fd69c40994b9d2b8a
Kapulara <bart.van.vliet@hotmail.com>
2020-03-21 14:37:35 +0100

    ✨Added another trigger at 14:30 everyday.

--- src/scraper.ts
+++ src/scraper.ts
@@ -7,7 +7,7 @@ import { CronJob } from 'cron';
 // Initialize git instance in root folder
 const git = require('simple-git/promise')();
 
-const municipalityJob = new CronJob('0 */2 * * *', async () => {
+async function municipalityJobTrigger() {
     const { today, time } = await triggerMunicipalityUpdate();
 
     if ( process.env.ENV === 'prod' ) {
@@ -15,12 +15,17 @@ const municipalityJob = new CronJob('0 */2 * * *', async () => {
     }
 
     console.log('Next trigger', municipalityJob.nextDate());
-}, null, true, 'Europe/Amsterdam');
+}
 
+const municipalityJob = new CronJob('0 */2 * * *', () => municipalityJobTrigger(), null, true, 'Europe/Amsterdam');
 municipalityJob.start();
+
+// Everyday at around 14:00 RIVM updates the data however. This can take some time so we trigger it around 14:30 again.
+const municipalityJob2 = new CronJob('30 14 * * *', () => municipalityJobTrigger(), null, true, 'Europe/Amsterdam');
+municipalityJob2.start();
 
 console.log('First trigger', municipalityJob.nextDate());
 
-if(process.env.ENV === 'dev' || process.env.FORCE_TRIGGER === 'true') {
-    municipalityJob.fireOnTick()
+if ( process.env.ENV === 'dev' || process.env.FORCE_TRIGGER === 'true' ) {
+    municipalityJob.fireOnTick();
 }
